5bdab1ee51d17da2e49146546db6f5bbcec1f198
bkkn_m <bakaikin@yandex.ru>
2022-02-17 10:27:00 +0300

    convert MainActivity into kotlin class

--- app/src/main/java/me/bkkn/gb_ktl_prj1/MainActivity.kt
+++ app/src/main/java/me/bkkn/gb_ktl_prj1/MainActivity.kt
@@ -1,14 +1,12 @@
-package me.bkkn.gb_ktl_prj1;
+package me.bkkn.gb_ktl_prj1
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.bkkn.gb_ktl_prj1.R
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
