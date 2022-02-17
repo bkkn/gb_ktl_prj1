8a72198c8cdd2ddde9ec318ee9f38854b0706298
bkkn_m <bakaikin@yandex.ru>
2022-02-17 11:53:27 +0300

    object Repo created which uses data class copy method

--- app/src/main/java/me/bkkn/gb_ktl_prj1/MainActivity.kt
+++ app/src/main/java/me/bkkn/gb_ktl_prj1/MainActivity.kt
@@ -6,12 +6,29 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    data class DummyData(val value1: String, val value2: Double)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.button)
             .setOnClickListener {
-                Toast.makeText(this, "Hello!", Toast.LENGTH_SHORT).show()
+                val data = Repo.getCopy();
+                Toast.makeText(
+                    this,
+                    "value of is " + data.value1 + " " + data.value2,
+                    Toast.LENGTH_SHORT
+                ).show()
             }
+    }
+
+
+    object Repo {
+        private val dummyData: DummyData = DummyData("pi", 3.14)
+
+        fun getCopy(): DummyData {
+            return dummyData.copy(value2 = 3.1415)
+        }
     }
 }
