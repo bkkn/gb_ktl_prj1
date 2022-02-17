750be0eb90b3233ff5a9138e88ad459e97e50f40
bkkn_m <bakaikin@yandex.ru>
2022-02-17 10:47:20 +0300

    button with onClick implemented

--- app/src/main/java/me/bkkn/gb_ktl_prj1/MainActivity.kt
+++ app/src/main/java/me/bkkn/gb_ktl_prj1/MainActivity.kt
@@ -1,12 +1,17 @@
 package me.bkkn.gb_ktl_prj1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import me.bkkn.gb_ktl_prj1.R
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button)
+            .setOnClickListener {
+                Toast.makeText(this, "Hello!", Toast.LENGTH_SHORT).show()
+            }
     }
 }
